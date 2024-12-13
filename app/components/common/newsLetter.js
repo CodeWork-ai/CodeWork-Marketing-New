ef2bf98f280aa8be37052c23a76af1318aa0513c
sainath <sainath.murthy@gmail.com>
2024-12-13 18:47:50 +0530

    subscribe

--- app/components/common/newsLetter.js
+++ app/components/common/newsLetter.js
@@ -1,51 +1,53 @@
 'use client'
 import React, { useState } from "react";
 import { FaTwitter, FaLinkedin, FaFacebook, FaInstagram } from "react-icons/fa";
+import { CoolMode } from "../../../components/ui/cool-mode";
 
 const NewsletterForm = () => {
   const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [message, setMessage] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        if (email && name) {
-            setIsLoading(true);
+    if (email) {
+      setIsLoading(true);
 
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/marketing_site/subscription_email`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ email }),
-                });
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/marketing_site/subscription_email`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email }),
+        });
 
-                const data = await response.json();
+        const data = await response.json();
 
-                if (response.ok) {
-                    setMessage(data.message || 'Thank you for subscribing!');
-                } else {
-                    setMessage(data.message || 'An error occurred. Please try again.');
-                }
-            } catch (error) {
-                setMessage('Network error. Please try again later.');
-            } finally {
-                setIsLoading(false);
-            }
+        if (response.ok) {
+          setMessage(data.detail || 'Thanks for subscribing!');
+          setEmail("")
         } else {
-            setMessage('Please fill out both fields.');
+          setMessage(data.detail || 'An error occurred. Please try again.');
         }
-    };
+      } catch (error) {
+        setMessage('Network error. Please try again later.');
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      setMessage('Please fill out both fields.');
+    }
+  };
 
 
   return (
     <div className="">
       <h3 className="text-lg font-semibold mb-4 text-gray-200">Subscribe to Our Newsletter</h3>
       <form onSubmit={handleSubmit} className="space-y-4">
-      <div>
+        {/* <div>
           <label className="block text-gray-400 text-sm lg:text-base 2xl:text-lg font-medium mb-1" htmlFor="email">
             Name <span className="text-red-500">*</span>
           </label>
@@ -57,7 +59,7 @@ const NewsletterForm = () => {
             required
             className="w-full p-2 bg-gray-800 text-white border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-        </div>
+        </div> */}
         <div>
           <label className="block text-gray-400 text-sm lg:text-base 2xl:text-lg font-medium mb-1" htmlFor="email">
             Email <span className="text-red-500">*</span>
@@ -83,14 +85,15 @@ const NewsletterForm = () => {
             Yes, subscribe me to your newsletter.
           </label>
         </div> */}
-        <button
-          type="submit"
-          className="w-full bg-[#fd6262] text-black py-2 px-4 rounded hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          disabled={isLoading}
-        >
-          {isLoading ? 'Submitting...' : 'Subscribe'}
-        </button>
-
+        <CoolMode>
+          <button
+            type="submit"
+            className="w-full bg-[#fd6262] text-black py-2 px-4 rounded hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            disabled={isLoading}
+          >
+            {isLoading ? 'Subscribing...' : 'Subscribe'}
+          </button>
+        </CoolMode>
         {message && (
           <p className="text-center text-sm mt-4 text-gray-400">{message}</p>
         )}
