c8b34c3eb9b0add1cb9cd3e75b320cd6698e0518
sainath <sainath.murthy@gmail.com>
2025-01-06 17:00:18 +0530

    Head change

--- app/service/page.js
+++ app/service/page.js
@@ -1,11 +1,14 @@
 import Services from "../components/service/service";
 import ServiceFooter from "../components/service/serviceFooter";
 import ServiceHeader from "../components/service/serviceHeader";
+import Head from "next/head";
 
 export default function Page() {
     return (
         <>
-            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({ "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [{ "@type": "ListItem", "position": 1, "name": "Home", "item": "https://www.codework.ai/" }, { "@type": "ListItem", "position": 2, "name": "Service", "item": "https://www.codework.ai/service" }] }), }} />
+            <Head>
+                <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({ "@context": "https://schema.org", "@type": "BreadcrumbList", "itemListElement": [{ "@type": "ListItem", "position": 1, "name": "Home", "item": "https://www.codework.ai/" }, { "@type": "ListItem", "position": 2, "name": "Service", "item": "https://www.codework.ai/service" }] }), }} />
+            </Head>
             <div className="text-white pb-5">
                 <ServiceHeader />
                 <Services />
