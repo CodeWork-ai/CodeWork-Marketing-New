f501f66f0bceec879f9fe7efe31257e7822468f9
Maharaja2026 <maharaja2632002@gmail.com>
2025-03-28 11:04:00 +0530

    visitor-logs prod issue fix

--- app/api/visitor-logs/route.js
+++ app/api/visitor-logs/route.js
@@ -5,11 +5,33 @@ import path from 'path';
 const credentialsPath = path.resolve(process.cwd(), 'codework-454409-1544ad4a5394.json');
 const credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf8'));
 
-const SPREADSHEET_ID = '1AoreO8IJ3H8RUsNmVdAu8d6J97azcYDWbCliIp3MS2M'; // Replace with your actual Spreadsheet ID
+const SPREADSHEET_ID = '1AoreO8IJ3H8RUsNmVdAu8d6J97azcYDWbCliIp3MS2M';
 
 export async function POST(request) {
   try {
-    const { ip, timestamp, page, userAgent, location, company } = await request.json();
+    const {
+      ip,
+      timestamp,
+      page,
+      userAgent,
+      location,
+      country,
+      countryCode,
+      region,
+      regionName,
+      city,
+      zip,
+      latitude,
+      longitude,
+      timeZone,
+      isp,
+      company,
+      as,
+      proxy,
+      screenResolution,
+      browserLanguage,
+      referrer,
+    } = await request.json();
 
     const auth = new google.auth.GoogleAuth({
       credentials,
@@ -20,10 +42,34 @@ export async function POST(request) {
 
     await sheets.spreadsheets.values.append({
       spreadsheetId: SPREADSHEET_ID,
-      range: 'Sheet1!A:F',
+      range: 'Sheet1!A:U',
       valueInputOption: 'RAW',
       resource: {
-        values: [[ip, timestamp, page, userAgent, location, company]],
+        values: [
+          [
+            ip,
+            timestamp,
+            page,
+            userAgent,
+            location,
+            country,
+            countryCode,
+            region,
+            regionName,
+            city,
+            zip,
+            latitude,
+            longitude,
+            timeZone,
+            isp,
+            company,
+            as,
+            proxy,
+            screenResolution,
+            browserLanguage,
+            referrer,
+          ],
+        ],
       },
     });
 
