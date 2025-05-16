dca0a0e1a2ba241f94625c83cbb28226a5d580c4
VinothXavier01 <vinothxavier78@gmail.com>
2025-05-16 15:25:58 +0530

    package file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true // Required for <Image> to work in static mode
+  }
+};
 
 export default nextConfig;
