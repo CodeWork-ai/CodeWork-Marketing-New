fdbaa87fb73d7a59cc9d026b3fabba5881e40512
nelson <xaviernelson121@gmail.com>
2025-05-16 16:59:25 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add custom webpack configurations here
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-      splitChunks: {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 244000,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      },
-    };
-    return config;
-  },
-  // Other Next.js config options
-  output: 'standalone',
-  compress: true,
-}
-
+const nextConfig = {};
 export default nextConfig;
