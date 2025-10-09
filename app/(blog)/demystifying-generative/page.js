9fb3cc3c0087e1221b839a167b6e36385813b92e
nelson <xaviernelson121@gmail.com>
2025-10-09 15:02:38 +0530

    feat(FAQs): add Computer Vision, Demystifying, and Underground mapping FAQs with metadata

--- app/(blog)/demystifying-generative/page.js
+++ app/(blog)/demystifying-generative/page.js
@@ -1,10 +1,12 @@
 import Blog19 from '@/app/components/blogs/blog19'
+import DemystifyingFaq from '@/app/components/FAQsforBlog/demystifyingFaq'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
         <Blog19 />
+        <DemystifyingFaq />
     </div>
   )
 }
